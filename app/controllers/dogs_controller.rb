9c09fb4d7f5ca42c8433425151c248c4d1058efa
Diana Liao <dijali@gmail.com>
2021-03-18 17:21:30 -0400

    Add edit for and index button

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,10 +1,11 @@
 class DogsController < ApplicationController
 
   def index
-    IF(params[button])
-    @dogs = Dog.all
-  else
-
+    if params[:view] == "sorted"
+      @dogs = Dog.all.sort_by { |dog| dog.employees.count }
+    else
+      @dogs = Dog.all
+    end
   end
 
   def show
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -13,9 +13,19 @@ class EmployeesController < ApplicationController
   end
 
   def create
-    emp_params
+    @employee = Employee.create(emp_params)
+    #@employee = Employee.create(first_name: emp_params[:first_name], last_name: emp_params[:last_name], alias: emp_params[:alias], office: emp_params[:office], dog_id: emp_params[:dog_id].to_i, title: emp_params[:title])
 
-    @employee = Employee.create(first_name: emp_params[:first_name], last_name: emp_params[:last_name], alias: emp_params[:alias], office: emp_params[:office], dog_id: emp_params[:dog_id].to_i, title: emp_params[:title])
+    redirect_to employee_path(@employee.id)
+  end
+
+  def edit
+    find_employee
+  end
+
+  def update
+    find_employee
+    @employee.update(emp_params)
 
     redirect_to employee_path(@employee.id)
   end
@@ -23,8 +33,13 @@ class EmployeesController < ApplicationController
   private
 
   def emp_params
+    params[:employee][:dog_id] = params[:employee][:dog_id].to_i
+    
     params.require(:employee).permit(:first_name, :last_name, :alias, :office, :dog_id, :title )
   end
 
+  def find_employee
+    @employee = Employee.find(params[:id])
+  end
 
 end
